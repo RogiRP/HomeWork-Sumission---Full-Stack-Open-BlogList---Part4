dea5e5e2e00c793a57e4dbe57b28c5c9a0048b50
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-04-30 17:43:41 -0600

    4.4: Funciones Auxiliares y Pruebas Unitarias, paso 2 - COMPLETADO

--- BlogList BackEnd/tests/list_helper.test.js
+++ BlogList BackEnd/tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
@@ -7,4 +7,35 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const blogs = [
+      {
+        likes: 5
+      }
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      { likes: 5 },
+      { likes: 3 },
+      { likes: 7 }
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 15)
+  })
+
 })
