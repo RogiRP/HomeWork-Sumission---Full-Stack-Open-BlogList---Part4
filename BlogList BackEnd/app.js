f97a546d0a3d8db2c34b36bb420f749e9a2de8f3
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-04-30 21:45:12 -0600

    Ejercicio: 4.17: Expansión de la Lista de Blogs, paso 5 - COMPLETADO

--- BlogList BackEnd/app.js
+++ BlogList BackEnd/app.js
@@ -3,19 +3,26 @@ const app = express()
 const cors = require('cors')
 const Blog = require('./models/blog')
 const usersRouter = require('./controllers/users')
+const User = require('./models/user')
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs)
 })
 
 app.post('/api/blogs', async (request, response) => {
-  const blog = new Blog(request.body)
+  const user = await User.findOne({})
+  
+  const blog = new Blog({...request.body, user: user._id})
   try{
     const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
   } catch (error) {
     response.status(400).json({error: error.message})
--- BlogList BackEnd/controllers/users.js
+++ BlogList BackEnd/controllers/users.js
@@ -39,4 +39,9 @@ router.post('/', async (request, response) => {
     }
 })
 
+router.get('/', async (request, response) => {
+    const users = await User.find({}).populate('blogs', {title: 1, author: 1, url: 1, likes: 1})
+    response.json(users)
+})
+
 module.exports = router
